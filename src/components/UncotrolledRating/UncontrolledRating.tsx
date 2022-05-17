e1cc759ad69d23da59ac09edd7e611a63a0f1504
a-butterman <apmyp.jezz@gmail.com>
2022-05-17 20:39:43 +0300

    rating v2

--- src/components/UncotrolledRating/UncontrolledRating.tsx
+++ src/components/UncotrolledRating/UncontrolledRating.tsx
@@ -12,11 +12,11 @@ export function UncontrolledRating() {
 
     return (
         <div>
-             <button style={buttonClass} onClick={ () => setValue(value = 1 ? 1 : 0) }><Star selected={value >= 1}/></button>
-             <button style={buttonClass} onClick={ () => setValue(value = 2 ? 2 : 0) }><Star selected={value >= 2}/></button>
-             <button style={buttonClass} onClick={ () => setValue(value = 3 ? 3 : 0) }><Star selected={value >= 3}/></button>
-             <button style={buttonClass} onClick={ () => setValue(value = 4 ? 4 : 0) }><Star selected={value >= 4}/></button>
-             <button style={buttonClass} onClick={ () => setValue(value = 5 ? 5 : 0) }><Star selected={value >= 5}/></button>
+             <button style={buttonClass} onClick={ () => setValue(1) }><Star selected={value >= 1}/></button>
+             <button style={buttonClass} onClick={ () => setValue(2) }><Star selected={value >= 2}/></button>
+             <button style={buttonClass} onClick={ () => setValue(3) }><Star selected={value >= 3}/></button>
+             <button style={buttonClass} onClick={ () => setValue(4) }><Star selected={value >= 4}/></button>
+             <button style={buttonClass} onClick={ () => setValue(5) }><Star selected={value >= 5}/></button>
         </div>
     )
 }
